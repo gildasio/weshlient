e89b5064441a5bbdc45dcccdf8ca114e10a7283d
Gildasio Junior <gildasiojunior@riseup.net>
2024-11-26 09:26:08 -0300

    Add request file parser
    I preferred the tool to work this way because the request and injection
    can be more generic to meet diverse needs. This way one can craft a HTTP
    request the specific way needed for an application yet exploiting it.

--- wshlient.py
+++ wshlient.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import readline
+import requests
 
 def command_clear(null=''):
     os.system("clear")
@@ -18,20 +20,59 @@ COMMANDS = {
 def special_commands(command):
     COMMANDS[command.split()[0]](command)
 
-def execute_command(command):
+def request_parser(fd):
+    method, path, version = fd.readline().split()
+    headers = {}
+    request = {}
+
+    lines = fd.readlines()
+    for line in lines:
+        if line in (b'\n', b'\r\n'):
+            break
+
+        headers[line.split(b': ')[0]] = line.split(b': ')[1].strip()
+
+    request['path'] = path
+    request['method'] = method
+    request['version'] = version
+    request['headers'] = headers
+
+    request['path'], *request['params'] = request['path'].split(b'?')
+    request['params'] = request['params'][0] or b''
+
+    request['body'] = ''
+    if method == b'POST':
+        request['body'] = lines[-1]
+
+    if request['path'].startswith(b'http'):
+        request['url'] = request['path']
+    else:
+        request['url'] = b'http://' + request['headers'][b'host'] + request['path']
+
+    return request
+
+def execute_command(request, command):
     if command == '':
         pass
     elif command.split()[0] in COMMANDS:
         special_commands(command)
     else:
+        print(request)
         print("TBD")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("req", help="File containing raw http request", type=str)
+    args = parser.parse_args()
+
+    with open(args.req, 'rb') as fd:
+        request = request_parser(fd)
+
     while True:
         try:
             PS1 = '$ '
             command = input(PS1)
-            execute_command(command)
+            execute_command(request, command)
         except (KeyboardInterrupt, EOFError) as e:
             command_exit()
 
